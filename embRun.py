adbf0ea435625654a8e3a4bba51b6baaba2a7df4
Álvaro Morales <moralesalvaro97@gmail.com>
2025-04-07 20:13:18 +0200

    TFM.docx manuscript for getting to uploded to remote repository.

--- embRun.py
+++ embRun.py
@@ -53,47 +53,6 @@ class Run:
         return lande_measure
 
 
-# def update_roleplay_answer(client, Answers, df, label, step):
-#     question = client.get_questions(Answers, roleplay_task_indication)
-#     answer = client.correct_questions(question, label)
-#     Answers += "|" + answer
-#     return Answers
-
-# def update_expo_abstract(client, Abstract, df, chunks, step):
-#     Abstract += chunks[step] + "|"
-#     return Abstract
-
-# def update_roleplay_baseline(client, Answers, df, label, step):
-#     return (utils.extractBaselineAnswer(df))
-
-# def run(embeddings_el, df, updating, update_fn, client, steps = 10):
-#   incrementText = ""
-#   lande_measure = []
-#   i = 0
-#   while i < steps:
-#       incrementText = update_fn(client, incrementText, df, updating, i)    
-#       tensor = torch.Tensor(
-#          embGet.generate_embeddings(incrementText)).unsqueeze(0)
-#       la = utils.lande_index(tensor, embeddings_el)
-#       print(la)
-#       lande_measure.append(la)
-#       i += 1
-#   return lande_measure
-
-# def run_conversation_intra(embeddings_el, df, updating, update_fn, client, steps = 10):
-#     incrementText = ""
-#     lande_measure = []
-#     i = 0
-#     while i < steps:
-#         incrementText = update_fn(client, incrementText, df, updating, i)    
-#         tensor = torch.Tensor(
-#             embGet.generate_embeddings(incrementText)).unsqueeze(0)
-#         la = utils.lande_intra_index(tensor.numpy()[0])
-#         print(la)
-#         lande_measure.append(la)
-#         i += 1
-#     return lande_measure
-
 def run_conversation(embeddings_el, generics : HFDataset, label, conversation_rounds = 10):
     client = Chat()
     runner_mayeutic = Run(update_mayeutic_answer, None, client, conversation_rounds)
@@ -103,10 +62,4 @@ def run_conversation(embeddings_el, generics : HFDataset, label, conversation_ro
     lande_intra = runner_mayeutic.run(None, label, 'intra')
     lande_base = runner_baseline.run(embeddings_el, label, 'intra')
     
-    # lande_convers = run(embeddings_el, generics.get_df(), label, 
-    #                     update_roleplay_answer, client, conversation_rounds)
-    # lande_intra = run_conversation_intra(embeddings_el, generics.get_df(), label,
-    #                     update_roleplay_answer, client, conversation_rounds)
-    # lande_base = run_conversation_intra(embeddings_el, generics.get_df(), label,
-    #                     update_roleplay_baseline, client, conversation_rounds)
     return lande_convers, lande_intra, lande_base
